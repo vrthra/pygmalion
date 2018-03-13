fb1926d6a946d3fa2fb0e89b3902cfab43519416
Rahul Gopinath <rahul@gopinath.org>
2018-03-13 15:08:32 +0100

    print grammar

--- bin/inferit.py
+++ bin/inferit.py
@@ -7,4 +7,5 @@ import pickle
 if __name__ == "__main__":
     grammarinfo = pickle.load(open(sys.argv[1], "rb" ))
     grammar = infer.infer_grammar(grammarinfo)
+    print(str(grammar))
     pickle.dump(grammar, open("%s.tmp" % sys.argv[1], "wb"))
