1abe9c054e25900993a27e2f9222ac49bc447923
Rahul Gopinath <rahul@gopinath.org>
2018-03-18 01:46:48 +0100

    update colors

--- bin/refineit.py
+++ bin/refineit.py
@@ -21,7 +21,7 @@ def nt_key_to_s(i):
     v = i.k
     return "[%s:%s]" % (v.func, v.var)
 
-def process(lststr):
+def char_class_process(lststr):
     i = 0
     last = None
     res = []
@@ -29,6 +29,9 @@ def process(lststr):
     while i < len(lststr):
         v = lststr[i]
         i = i+1
+        if v == '+':
+            res.append('[<plus>]')
+            continue
         if len(res) == 0:
             res.append(v)
             continue
@@ -43,6 +46,14 @@ def process(lststr):
             else:
                 res.append(v)
     return ''.join(res)
+
+def to_str(i):
+    if isinstance(i, str):
+        return i
+    elif isinstance(i, list):
+        return ''.join(i)
+    else:
+        return type(i).__name__ + ':' + str(i)
 
 if __name__ == "__main__":
     fin = sys.stdin.buffer if len(sys.argv) < 2 else open(sys.argv[1], 'rb')
@@ -71,19 +82,19 @@ if __name__ == "__main__":
                 c = r.comparisons[pos]
                 my_str.extend(["\t"] +
                         [
-                         yellow('EQ_y:') + repr(''.join([str(i) for i in c['seq']]))     if c['seq'] else '',
-                         yellow('NE_y:') + repr(''.join([str(i) for i in c['sne']]))     if c['sne'] else '',
-                         yellow('IN_y:') + repr(''.join([''.join(i) for i in c['sin']])) if c['sin'] else '',
-                         yellow('NI_y:') + repr(''.join([''.join(i) for i in c['sni']])) if c['sni'] else '',
-                         yellow('EQ_X:') + repr(''.join([str(i) for i in c['feq']]))     if c['feq'] else '',
-                         yellow('NE_x:') + repr(''.join([str(i) for i in c['fne']]))     if c['fne'] else '',
-                         yellow('IN_x:') + repr(''.join([''.join(i) for i in c['fin']])) if c['fin'] else '',
-                         yellow('NI_x:') + repr(''.join([''.join(i) for i in c['fni']])) if c['fni'] else '',
+                         yellow('EQ_y:') + repr(''.join([to_str(i) for i in c['seq']]))     if c['seq'] else '',
+                         yellow('NE_y:') + repr(''.join([to_str(i) for i in c['sne']]))     if c['sne'] else '',
+                         yellow('IN_y:') + repr(''.join([to_str(i) for i in c['sin']])) if c['sin'] else '',
+                         yellow('NI_y:') + repr(''.join([to_str(i) for i in c['sni']])) if c['sni'] else '',
+                         yellow('EQ_X:') + repr(''.join([to_str(i) for i in c['feq']]))     if c['feq'] else '',
+                         yellow('NE_x:') + repr(''.join([to_str(i) for i in c['fne']]))     if c['fne'] else '',
+                         yellow('IN_x:') + repr(''.join([to_str(i) for i in c['fin']])) if c['fin'] else '',
+                         yellow('NI_x:') + repr(''.join([to_str(i) for i in c['fni']])) if c['fni'] else '',
                          ])
             else:
                 pass
                 #my_str.extend([''])
-            newr.add(process(my_str))
+            newr.add(char_class_process(my_str))
     x = g.Grammar(newg)
     if config.Compress_Grammar:
         x.compress()
