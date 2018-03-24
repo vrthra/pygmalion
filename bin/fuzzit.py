bac488c168f602de51536ab1f88ce9ced56621eb
Rahul Gopinath <rahul@gopinath.org>
2018-03-24 22:13:42 +0100

    update

--- bin/fuzzit.py
+++ bin/fuzzit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env
 import sys
+import os
 import pickle
 import pygmalion.fuzz as fuzz
 
 if __name__ == "__main__":
+    max_sym = int(os.getenv('MAXSYM') or '100')
     fin = sys.stdin.buffer if len(sys.argv) < 2 else open(sys.argv[1], 'rb')
     fout = sys.stdout if len(sys.argv) < 2 else open("%s.tmp" % sys.argv[1], 'wb')
     grammar = pickle.load(fin)
-    print(fuzz.produce(grammar, 100))
+    print(fuzz.produce(grammar, max_sym))
