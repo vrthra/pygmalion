7fb4b4e9e47b93cc7b54b750caf700a4f4583a52
Rahul Gopinath <rahul@gopinath.org>
2018-03-25 19:50:42 +0200

    array

--- subjects/array.py
+++ subjects/array.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python3
+# An example where there is more to generation than fixing the last char
+# the mezzanine validation of equal closing brackets.
 
 def inputs():
-    return ['[1]',
-            '[1, 2]',
-            '[1, 2, 3]'
+    return ['(1)',
+            '(1(2))',
+            '(1(2)3)',
+            '((12)3)',
+            '(((1)2)3)',
             ]
 
+
+def parens(xs):
+    stack = [[]]
+    while True:
+        x, xs = xs[0], xs[1:]
+        if x == '(':
+            stack[-1].append([])
+            stack.append(stack[-1][-1])
+        elif x == ')':
+            stack.pop()
+            if not stack:
+                raise Exception('error: opening bracket is missing')
+                #raise ValueError('error: opening bracket is missing')
+        elif x.in_('0123456789'):
+            stack[-1].append(x)
+        else:
+            raise Exception('error: Only numbers')
+        if xs == '':
+            break
+    if len(stack) > 1:
+        raise Exception('error: closing bracket is missing')
+        #raise ValueError('error: closing bracket is missing')
+    return stack.pop()
+
 def main(s):
-    if s[0] != '[': return
-    if s[-1] != ']': return
-    vals = s[1:-1].split(',')
-    nums = []
-    for v in vals:
-        num = v.strip()
-        nums.append(num)
+    return parens(s)
 
 if __name__ == '__main__':
     import taintedstr
