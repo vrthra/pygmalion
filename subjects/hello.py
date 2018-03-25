b19bab0141d85885c59fe596c7b20473c3b9bf91
Rahul Gopinath <rahul@gopinath.org>
2018-03-25 05:53:48 -0700

    update

---
+++ subjects/hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def inputs():
+    return ['hello', 'HELLO']
+
+def main(s):
+    h,s = s[0], s[1:]
+    assert s != ''
+    assert h == 'h' or h == 'H'
+    e,s = s[0], s[1:]
+    assert e == 'e' or e == 'E'
+    assert s != ''
+    l1, s = s[0], s[1:]
+    assert l1 == 'l' or l1 == 'L'
+    assert s != ''
+    l2, s = s[0], s[1:]
+    assert l2 == 'l' or l2 == 'L'
+    assert s != ''
+    o, s = s[0], s[1:]
+    assert o == 'o' or o == 'O'
+    assert s == ''
+    return h + e + l1 + l2 + o
+
+if __name__ in '__main__':
+    import taintedstr
+    for i in inputs():
+        print(main(taintedstr.tstr(i)))
+
+
