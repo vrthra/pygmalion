bc9e8ca747d3ce570959878f60704c82f02c0fb0
Rahul Gopinath <rahul@gopinath.org>
2018-04-11 04:04:10 -0700

    update

--- bin/inferit.py
+++ bin/inferit.py
@@ -12,8 +12,8 @@ sys.setrecursionlimit(0x100000)
 if __name__ == "__main__":
     fin = sys.stdin.buffer if len(sys.argv) < 2 else open(sys.argv[1], 'rb')
     fout = sys.stdout.buffer if len(sys.argv) < 2 else open("%s.tmp" % sys.argv[1], 'wb')
-    grammarinfo = pickle.load(fin)
-    grammar = infer.infer_grammar(grammarinfo)
+    parse_trees = pickle.load(fin)
+    grammar = infer.infer_grammar(parse_trees)
     if os.getenv('DEBUG'):
         print(str(grammar), file=sys.stderr)
     pickle.dump(grammar, fout)
