7ced21a328286bba9d325414e86d13dff098edf9
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2018-04-23 03:14:07 -0700

    count records

--- bin/showpickle.py
+++ bin/showpickle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import pickle
 import sys
-import jsonpickle
+count = 0
 with open(sys.argv[1],"rb") as tf:
     try:
         while True:
             p = pickle.load(tf)
+            count += 1
             #print(jsonpickle.encode(p))
             print(p)
     except:
         pass
+print(count)
